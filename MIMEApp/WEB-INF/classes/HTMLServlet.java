0e0363cddc069540762a6c48918cd3f0a4183adc
Know-Program <83252909+Know-Program@users.noreply.github.com>
2021-04-27 04:41:54 +0530

    MIMEApp

---
+++ MIMEApp/WEB-INF/classes/HTMLServlet.java
@@ -0,0 +1,32 @@
+// HTMLServlet.java
+package com.kp.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+public class HTMLServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res)
+	            throws ServletException, IOException {
+        // set content type
+	    res.setContentType("text/html");
+
+	    // get printwriter 
+	    PrintWriter pw = res.getWriter();
+
+	    // write message to response object
+	    pw.println(
+	    	       "<table border='1' bgcolor='cyan' align='center'>"+
+	    	       "<tr><th>Language</th> <th>Developer</th></tr>"+
+	    	       "<tr><td>C</td> <td>Dennis M. Ritchie</td></tr>"+
+	    	       "<tr><td>C++</td> <td>Bjarne Stroustrup</td></tr>"+
+	    	       "<tr><td>Java</td> <td>James Gosling</td></tr>"+
+	    	       "<tr><td>Python</td> <td>Guido van Rossum</td></tr>"+
+	    	       "</table>"
+	    	);
+
+	    // close stream
+	    pw.close();
+    }  
+}
---
+++ MIMEApp/WEB-INF/classes/PlainServlet.java
@@ -0,0 +1,32 @@
+// PlainServlet.java
+package com.kp.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+public class PlainServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res)
+	            throws ServletException, IOException {
+        // set content type
+	    res.setContentType("text/plain");
+
+	    // get printwriter 
+	    PrintWriter pw = res.getWriter();
+
+	    // write message to response object
+	    pw.println(
+	    	       "<table border='1' bgcolor='cyan' align='center'>"+
+	    	       "<tr><th>Language</th> <th>Developer</th></tr>"+
+	    	       "<tr><td>C</td> <td>Dennis M. Ritchie</td></tr>"+
+	    	       "<tr><td>C++</td> <td>Bjarne Stroustrup</td></tr>"+
+	    	       "<tr><td>Java</td> <td>James Gosling</td></tr>"+
+	    	       "<tr><td>Python</td> <td>Guido van Rossum</td></tr>"+
+	    	       "</table>"
+	    	);
+
+	    // close stream
+	    pw.close();
+    }  
+}

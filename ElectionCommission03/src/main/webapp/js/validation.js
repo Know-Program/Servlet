483de0a8001f9eb406449a1ed607a1d4d08cd6a6
Know-Program <Know-Program@KnowProgram>
2021-05-03 17:21:55 +0530

    With getElementById and span - Client Side Form Validation

---
+++ ElectionCommission03/src/main/webapp/js/validation.js
@@ -0,0 +1,35 @@
+/**
+ * JavaScript code for Client Side
+ * Form validation.
+ */
+function validate(frm) {
+	//read from data
+	var name = frm.pname.value;
+	var age = frm.page.value;
+	// write client side form validation logic
+	if (name == "") {
+		document.getElementById("pnameErr").innerHTML =
+			"<b>Person name is required</b>";
+		frm.pname.focus();
+		return false;
+	}
+	if (age == "") {
+		document.getElementById("pageErr").innerHTML =
+			"<b>Person age is required</b>";
+		frm.page.focus();
+		return false;
+	} else if (isNaN(age)) {
+		document.getElementById("pageErr").innerHTML =
+			"<b>Person age must be numeric value</b>";
+		frm.page.focus();
+		return false;
+	} else {
+		if (age <= 0 || age > 125) {
+			document.getElementById("pageErr").innerHTML =
+				"<b>Person age must be between 1 to 125</b>";
+			frm.page.focus();
+			return false;
+		}
+	}
+	return true;
+}
